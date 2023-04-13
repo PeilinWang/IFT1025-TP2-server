ffc76e32c9d8e4422ac8220b2b1666b9a90ba7ec
sosoo24 <129554581+sosoo24@users.noreply.github.com>
2023-04-13 12:12:59 -0500

    Add session Message and connect to server

--- src/main/java/client/LineClient.java
+++ src/main/java/client/LineClient.java
@@ -1,3 +1,16 @@
 public class LineClient {
-  
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 1337;
+
+    public static void main(String[] args) {
+        System.out.println("*** Bienvenue au portail d'inscription de cours de l'Udem ***");
+        LineClient client = new LineClient();
+        client.sessionsMessage();
+    }
+
+    public void sessionsMessage() {
+        System.out.println("Veuillez choisir la session pour laquelle vous voulez consulter la liste de cours");
+        System.out.println("1.Automne" + "\n" + "2.Hiver" + "\n" + "3.Été");
+        System.out.print("> Choix: ");
+    }
 }
